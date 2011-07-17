50120afc0d84ad0608c05c9bc84166896988f94e
Rafael Santos <formigarafa@gmail.com>
2011-07-16 23:19:54 -0300

    rails e Gemfile, gitigoring .rvmrc

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'mysql2', '<0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockit::Application.config.secret_token = '2fa6a539d7e020acd921169e4a855641ce1d747d678ea14433de35ff69cd25693c308c27688ee2fc3780ae708463bf4283f6b1c13d2f6dee457fcddd31f5512e'
