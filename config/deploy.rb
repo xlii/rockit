71affbf8e7580f860091b4e3b9d019a7706fdc28
Rafael Marcelino dos Santos <formigarafa@gmail.com>
2011-12-01 23:55:34 -0200

    inclusão de tarefa de backup no processo de deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,30 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+set :backup_path do
+  "#{shared_path}/backup/#{release_name}"
+end
+
+namespace :data do
+  desc "store a backup of user's data"
+  namespace :backup do 
+    task :default do
+      database
+    end
+    
+    desc "store a database dump (run after update code)"
+    task :database, :roles => :db, :only => { :primary => true } do
+      run "mkdir -p #{backup_path}"
+      filename = "#{backup_path}/mysqldump.sql.bz2"
+      text = capture "cat #{deploy_to}/current/config/database.yml"
+      yaml = YAML::load(text)
+
+      run "mysqldump -h #{yaml['production']['host']} -u #{yaml['production']['username']} -p #{yaml['production']['database']} | bzip2 -c > #{filename}" do |ch, stream, out|
+        ch.send_data "#{yaml['production']['password']}\n" if out =~ /^Enter password:/
+      end
+    end
+  end
+end
+
+after "deploy:update_code", "data:backup"
