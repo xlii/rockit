0a80bde191ff74a59c2ad0dae16eb6edfcb5ddeb
Adriana Silva <=>
2011-12-27 15:55:58 -0200

    ajustado layout da pagina de login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   def user_for_paper_trail
     current_admin_user
   end
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? and params[:action] == 'new'
+    	puts 'action'+params.inspect
+      "active_admin_logged_out"
+    else
+      "application"
+    end
+  end
+ 
 end
