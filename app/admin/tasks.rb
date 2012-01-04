644c63f025674f13a531e614af6d8d5af0319ef0
Adriana Silva <=>
2012-01-04 11:32:39 -0200

    ajustes nas colunas a serem exibidas na listagem de tasks

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Task do
-  
+  index do
+  	column :id
+    column :project
+    column :name do |task|
+    	link_to task.name, admin_task_path(task)
+    end
+    column :resolution
+    column :priority    
+    column :created_at
+    default_actions
+  end
 end
